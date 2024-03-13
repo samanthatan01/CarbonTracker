67396f08d45a79b4d43532e6c2a61811c239c94c
samanthatan01 <samanthatan6496@gmail.com>
2024-03-13 13:32:06 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from "react";
-import Display from "./components/Display";
+import React, { Suspense } from "react";
+import { Navigate, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Member from "./pages/Member";
+import List from "./pages/List";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <div className="row">
-        <div className="col-sm-3">Choose vehicle type</div>
-        <Display className="col-sm-3"></Display>
-      </div>
+      <Suspense fallback={<h2>loading...</h2>}>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/main" />} />
+          <Route path="main" element={<Main />} />
+          <Route path="member/:id" element={<Member />} />
+          <Route path="member/list" element={<List />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
